5b973866e9b4697652b9e12e5a2fb0edcb1e6378
“Patrick <pctennislessons@gmail.com>
2019-09-17 01:17:57 -0600

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,44 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
+
+  cart.each_with_object({}){ |item, hash| 
+    item.each{|name, content| 
+      hash[name] = content
+      if  hash[name][:count] == nil
+        hash[name][:count] =  1
+      else
+        hash[name][:count] +=  1
+      end
+    }
+  }
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.map{ |coupon| 
+    if cart.keys.include?(coupon[:item]) && (cart[coupon[:item]][:count] >= coupon[:num])
+      cart["#{coupon[:item]} W/COUPON"] = {
+          :price => coupon[:cost] / coupon[:num], 
+          :clearance => cart[coupon[:item]][:clearance], 
+          :count => cart[coupon[:item]][:count]/coupon[:num] * coupon[:num]
+        }
+        cart[coupon[:item]][:count] = cart[coupon[:item]][:count]  - (cart[coupon[:item]][:count]/coupon[:num] * coupon[:num]) 
+    end
+    }
+
+    cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.map{|item, sales_info| 
+    # binding.pry
+    if sales_info[:clearance] == true
+      cart[item][:price] = (cart[item][:price]*0.8).round(2)
+    end
+  }
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidate_cart(cart)
+
 end
